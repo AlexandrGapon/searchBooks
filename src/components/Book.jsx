105e20ff8334127e55822c487c52cecf5195f300
AlexandrGapon <gapon769@gmail.com>
2021-09-10 12:27:58 +0300

    add book component

---
+++ src/components/Book.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Book = ({ author, title, cover, isbn, ...props }) => {
+
+    const openModal = () => {
+        console.log(isbn)
+    }
+
+    return (
+        <div className='book' onClick={() => {openModal()}}  >
+            <img src={`https://covers.openlibrary.org/b/id/${cover}-M.jpg`} alt='Book Cover' className='book__pic' />
+            <div className='book__items'>
+                <span className='book__author'>Author: {author}</span>
+                <span className='book__title'>Title: {title}</span>
+            </div>
+        </div>
+    )
+}
+
+export default Book
